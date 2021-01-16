92975a2275856790fcc47740315edb2b3894f8eb
Dan Vassallo <dannyvassallo@users.noreply.github.com>
2021-01-16 04:32:44 +0000

    added firebase (#3)
    * added firebase
    
    * lint
    
    * add run mutation
    
    * no useState
    
    * Update .gitignore
    
    * updated to handle player state
    
    * firebase config
    
    * fixes
    
    * remove examples
    
    * updating envs
    
    * actually working now
    
    * lint
    
    * remove extra p tag
    
    * add space to conditional
    
    * update secrets
    
    * hoist up env
    
    * move run under build with envs
    
    * comment out build step
    
    Co-authored-by: Danny <46854315+DanFCo@users.noreply.github.com>

--- pages/watch/[slug].tsx
+++ pages/watch/[slug].tsx
@@ -1,15 +1,42 @@
 import { useRouter } from 'next/router'
-import React, { FC } from 'react'
+import React, { FC, useState, useEffect } from 'react'
+import firebase from 'firebase/app'
+import 'firebase/database'
+import { config } from '../../firebaseConfig'
+import { useObjectVal } from 'react-firebase-hooks/database'
 
+if (!firebase.apps.length) {
+  firebase.initializeApp(config)
+} else {
+  firebase.app()
+}
 type Props = {
 }
 
 const Watch: FC<Props> = () => {
   const { query } = useRouter()
+  const [value, loading, error] = useObjectVal(firebase.database().ref(`player/${query?.slug}`))
+  const isPlaying = value && !value['playing']
+
   if (!query?.slug) return <div />
 
   return (
-    <>Watch party Page for &quot;{query.slug}&quot;!</>
+    <div>
+      {error && <strong>Error: {error}</strong>}
+      {loading && <span>Loading...</span>}
+      {!loading && (
+        <>
+          <button onClick={() => {
+            firebase.database().ref(`player/${query?.slug}`).set({
+              'playing': !!isPlaying
+            })
+          }}>
+            { isPlaying ? 'Pause' : 'Play' }
+          </button>
+          <p>{`Playing: ${!!isPlaying}`}</p>
+        </>
+      )}
+    </div>
   )
 }
 

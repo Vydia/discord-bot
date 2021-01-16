533295075ddc01d2665f54f65f647702e0e11cd5
Paul Santos <paulmichael.a.santos@gmail.com>
2021-01-15 21:13:52 -0500

    test

--- pages/watch/[slug].tsx
+++ pages/watch/[slug].tsx
@@ -7,7 +7,7 @@ type Props = {
 const Watch: FC<Props> = () => {
   const { query } = useRouter()
   if (!query?.slug) return <div />
-
+  console.warn('dan idiot')
   return (
     <>Watch party Page for &quot;{query.slug}&quot;!</>
   )

b80c9e9051349b8c693082f2a5ecb890d5e72002
TSMMark <mark.allen@vydia.com>
2021-01-15 22:31:25 -0500

    homescreen text box

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,22 @@
 import Head from 'next/head'
-import Link from 'next/link'
-import Img from 'next/image'
-import React, { FC } from 'react'
+import React, { FC, useRef } from 'react'
+import Router from 'next/router'
+
+function getVideoId (url: string): string | void {
+  const regExp = /^.*(youtu\.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/
+  const match = url.match(regExp)
+
+  if (match && match[2].length === 11) {
+    return match[2]
+  }
+}
 
 type Props = {
 }
 
 const Home: FC<Props> = () => {
+  const inputRef = useRef<HTMLInputElement>(null)
+
   return (
     <div>
       <Head>
@@ -18,7 +28,23 @@ const Home: FC<Props> = () => {
       </Head>
 
       <main className="relative flex flex-col items-center justify-center min-h-screen bg-stars bg-black text-white">
-        Hello!
+        <h1 className="text-xl p-4">Enter a YouTube link to start or join a Watch Party!</h1>
+        <div className="max-w-xl w-screen">
+          <form onSubmit={(event) => {
+            event.preventDefault()
+            const url = inputRef.current.value
+            const videoId = getVideoId(url)
+
+            if (videoId) {
+              Router.push(`/watch/${videoId}`)
+            } else {
+              alert('Oops! Try entering a valid YouTube URL this time.')
+            }
+          }}>
+            <input className="block w-full my-4 p-4" placeholder="Paste YouTube link here" defaultValue="" ref={(c: HTMLInputElement) => inputRef.current = c} />
+            <button className="block w-full p-4">Go</button>
+          </form>
+        </div>
       </main>
     </div>
   )
--- tests/Index.test.tsx
+++ tests/Index.test.tsx
@@ -1,12 +1,27 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
-import '@testing-library/jest-dom/extend-expect'
+import userEvent from '@testing-library/user-event'
 import Index from '../pages/index'
+import '@testing-library/jest-dom/extend-expect'
+import Router from 'next/router'
+
+const mockedRouterPush = Router.push as jest.Mock<any>
 
 describe('Index', () => {
   test('displays the heading', async () => {
+    let path: string = ''
+
+    mockedRouterPush.mockImplementation((newPath: string) => {
+      path = newPath
+    })
+
     render(<Index />)
 
-    screen.getByText('Hello!')
+    screen.getByText('Enter a YouTube link to start or join a Watch Party!')
+
+    userEvent.type(screen.getByRole('textbox'), 'https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+    userEvent.click(screen.getByRole('button'))
+
+    expect(path).toBe('/watch/dQw4w9WgXcQ')
   })
 })

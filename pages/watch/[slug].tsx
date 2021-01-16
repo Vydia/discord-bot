21d514a64de35ce8adf4538d995cf2e2065b5915
dannyvassallo <danielvassallo87@gmail.com>
2021-01-15 22:33:56 -0500

    actually working now

--- pages/watch/[slug].tsx
+++ pages/watch/[slug].tsx
@@ -1,5 +1,5 @@
 import { useRouter } from 'next/router'
-import React, { FC, useState } from 'react'
+import React, { FC, useState, useEffect } from 'react'
 import firebase from 'firebase/app'
 import 'firebase/database'
 import { config } from '../../firebaseConfig'
@@ -17,6 +17,7 @@ const Watch: FC<Props> = () => {
   const { query } = useRouter()
   const [value, loading, error] = useObjectVal(firebase.database().ref(`player/${query?.slug}`));
   const isPlaying = value && !Boolean(value["playing"])
+
   if (!query?.slug) return <div />
 
   return (
@@ -28,12 +29,12 @@ const Watch: FC<Props> = () => {
           <>
             <button onClick={() => {
               firebase.database().ref(`player/${query?.slug}`).set({
-                "playing": isPlaying
+                "playing": !!isPlaying
               })
             }}>
               { isPlaying ? "Pause" : "Play" }
             </button>
-            <p>{`Playing: ${isPlaying}`}</p>
+            <p>{`Playing: ${!!isPlaying}`}</p>
           </>
         )}
       </p>

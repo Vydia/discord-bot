b2e4e152b2a3651f082e23b7a74ab205611c1b94
dannyvassallo <danielvassallo87@gmail.com>
2021-01-15 21:32:10 -0500

    add run mutation

--- pages/watch/[slug].tsx
+++ pages/watch/[slug].tsx
@@ -1,10 +1,11 @@
 import { useRouter } from 'next/router'
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
 import firebase from 'firebase/app'
 import 'firebase/database'
 import {
   FirebaseDatabaseProvider,
-  FirebaseDatabaseNode
+  FirebaseDatabaseNode,
+  FirebaseDatabaseMutation
 } from '@react-firebase/database'
 import { config } from '../../firebaseConfig'
 
@@ -13,17 +14,33 @@ type Props = {
 
 const Watch: FC<Props> = () => {
   const { query } = useRouter()
+  const [playerState, setPlayerState] = useState(null)
   if (!query?.slug) return <div />
 
   return (
     <FirebaseDatabaseProvider firebase={firebase} {...config}>
       <FirebaseDatabaseNode
-        path="player/"
+        path={`player/${query?.slug}`}
         limitToFirst={1}
         orderByKey
       >
         {d => {
           return (<>
+            <FirebaseDatabaseMutation type="set" path={`player/${query?.slug}`}>
+              {({ runMutation }) => {
+                return (
+                  <div>
+                    <button
+                      onClick={async () => {
+                        const { key } = await runMutation({ playing: "test" });
+                      }}
+                    >
+                      Play
+                    </button>
+                  </div>
+                );
+              }}
+            </FirebaseDatabaseMutation>
             <p>{JSON.stringify(d.value)}</p>
             <p>Watch party Page for &quot;{query.slug}&quot;!</p>
           </>)

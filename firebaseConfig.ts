07ff6062372216a613fb77b4a2c3d4dfb2a3b844
Danny <46854315+DanFCo@users.noreply.github.com>
2021-01-15 22:15:15 -0500

    firebase config

---
+++ firebaseConfig.ts
@@ -0,0 +1,10 @@
+export const config = {
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  databaseURL: process.env.databaseURL,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId,
+}

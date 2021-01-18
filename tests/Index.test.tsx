94bd6e02fc59078b81a684ce448e8e123cdad25f
TSMMark <mark.allen@vydia.com>
2021-01-17 23:44:33 -0500

    allow entering a youtube video id

--- tests/Index.test.tsx
+++ tests/Index.test.tsx
@@ -25,6 +25,23 @@ describe('Index', () => {
     expect(path).toBe('/watch/dQw4w9WgXcQ?hasControl=true')
   })
 
+  test('when given a valid YouTube Video ID it navigates to the /watch/XXX path for a valid YouTube Video ID', async () => {
+    let path = ''
+
+    mockedRouterPush.mockImplementation((newPath: string) => {
+      path = newPath
+    })
+
+    render(<Index />)
+
+    screen.getByText('Enter a YouTube link to start or join a Watch Party!')
+
+    userEvent.type(screen.getByRole('textbox'), 'dQw4w9WgXcQ')
+    userEvent.click(screen.getByRole('button'))
+
+    expect(path).toBe('/watch/dQw4w9WgXcQ?hasControl=true')
+  })
+
   test('when given an invalid YouTube Video URL it shows a browser alert', async () => {
     let path = ''
 
@@ -41,6 +58,12 @@ describe('Index', () => {
     userEvent.click(screen.getByRole('button'))
 
     expect(path).toBe('')
-    expect(window.alert).toBeCalledWith('Oops! Try entering a valid YouTube URL this time.')
+    expect(window.alert).toBeCalledWith('Oops! Try entering a valid YouTube URL or Video ID this time.')
+
+    userEvent.type(screen.getByRole('textbox'), 'dQw4w9WgXcQ1') // Invalid because all YouTube Video IDs have 11 characters.
+    jest.spyOn(window, 'alert').mockImplementation(() => {})
+    userEvent.click(screen.getByRole('button'))
+
+    expect(path).toBe('')
   })
 })

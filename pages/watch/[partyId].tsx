9812cbdb49fb4824383634f88f50c0b9fc06103d
TSMMark <mark.allen@vydia.com>
2021-10-31 16:01:24 -0400

    fix player size

--- pages/watch/[partyId].tsx
+++ pages/watch/[partyId].tsx
@@ -7,8 +7,8 @@ const Watch: FC = () => {
   if (!query?.partyId) return <div />
 
   return <main className="relative flex flex-col items-center justify-center min-h-screen bg-stars bg-black text-white">
-    <div className="bg-black">
-      <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between flex-col">
+    <div className="bg-black w-full">
+      <div className="max-w-7xl md:w-10/12 md:w-8/12 mx-auto py-12 px-4 sm:px-6 lg:py-2 lg:px-8 lg:flex lg:items-center lg:justify-between flex-col">
         <YouTubeVideoPlayer partyId={query?.partyId as string} />
       </div>
     </div>
